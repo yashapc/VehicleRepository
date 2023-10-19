488b94e000e606413a0a5aef24dadf15dae3c725
yashapc <yashchaudhari421@gmail.com>
2023-10-19 17:33:41 +0530

    dao class commit

--- src/main/java/com/cg/dao/VehicleDao.java
+++ src/main/java/com/cg/dao/VehicleDao.java
@@ -1,5 +1,13 @@
 package com.cg.dao;
 
+import java.util.List;
+
+import com.cg.entity.Vehicle;
+
 public interface VehicleDao {
+
+	String addEmployee(Vehicle v);
+
+	List<Vehicle> getAllVehicle();
 
 }
