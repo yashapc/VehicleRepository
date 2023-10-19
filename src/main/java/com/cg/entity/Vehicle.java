ec10c76206ba93d7a2e86abf6686bdfa11e5690d
yashapc <yashchaudhari421@gmail.com>
2023-10-19 17:08:51 +0530

    Initial commit

---
+++ src/main/java/com/cg/entity/Vehicle.java
@@ -0,0 +1,37 @@
+package com.cg.entity;
+
+public class Vehicle {
+	String vehicleName;
+	String vehicleNumber;
+
+	public Vehicle() {
+	}
+
+	public Vehicle(String vehicleName, String vehicleNumber) {
+		super();
+		this.vehicleName = vehicleName;
+		this.vehicleNumber = vehicleNumber;
+	}
+
+	public String getVehicleName() {
+		return vehicleName;
+	}
+
+	public void setVehicleName(String vehicleName) {
+		this.vehicleName = vehicleName;
+	}
+
+	public String getVehicleNumber() {
+		return vehicleNumber;
+	}
+
+	public void setVehicleNumber(String vehicleNumber) {
+		this.vehicleNumber = vehicleNumber;
+	}
+
+	@Override
+	public String toString() {
+		return "Vehicle [vehicleName=" + vehicleName + ", vehicleNumber=" + vehicleNumber + "]";
+	}
+
+}
