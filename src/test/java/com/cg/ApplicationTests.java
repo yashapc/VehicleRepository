2c09111c20260df29a051993582b84141f7aad26
yashapc <yashchaudhari421@gmail.com>
2023-10-20 12:26:21 +0530

    initial commit

--- src/test/java/com/cg/ApplicationTests.java
+++ src/test/java/com/cg/ApplicationTests.java
@@ -3,10 +3,10 @@ package com.cg;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class ApplicationTests {
 
-	@Test
+	
 	void contextLoads() {
 	}
 
